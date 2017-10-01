62118a22e429bec6560378087a09bdbe3d17f43b
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-10-02 01:50:46 +0200

    Finalize the basic structure of RecycleView

--- app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
@@ -2,12 +2,26 @@ package name.juhasz.judit.udacity.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mMoviesRecycleView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoviesRecycleView = (RecyclerView) findViewById(R.id.rv_movies);
+
+        final MovieAdapter adapter = new MovieAdapter();
+        mMoviesRecycleView.setAdapter(adapter);
+
+        final int spanCount = 2;
+        GridLayoutManager layoutManager = new GridLayoutManager(this, spanCount);
+        mMoviesRecycleView.setLayoutManager(layoutManager);
     }
 }
