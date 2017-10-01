c5502ca1bd9af4ae363f9d2c719fe52adaff4496
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-10-02 00:20:18 +0200

    Create model for movie that will be used by RecycleView

---
+++ app/src/main/java/name/juhasz/judit/udacity/popularmovies/Movie.java
@@ -0,0 +1,18 @@
+package name.juhasz.judit.udacity.popularmovies;
+
+/**
+ * Created by Judit on 10/2/2017.
+ */
+
+public class Movie {
+
+    private String mTitle;
+
+    public Movie(String title) {
+        mTitle = title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+}
