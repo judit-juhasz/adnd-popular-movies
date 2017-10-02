b02571ec93cf365e891a39c89cc701dd4dd843a7
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-10-02 23:25:36 +0200

    Add menu to choose between different movie lists

--- app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
@@ -1,11 +1,18 @@
 package name.juhasz.judit.udacity.popularmovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView mMoviesRecycleView;
 
@@ -24,4 +31,29 @@ public class MainActivity extends AppCompatActivity {
         GridLayoutManager layoutManager = new GridLayoutManager(this, spanCount);
         mMoviesRecycleView.setLayoutManager(layoutManager);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu_item_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = MainActivity.this;
+        final int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.action_switch_most_popular:
+                Toast.makeText(context, "Most popular selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_switch_highest_rated:
+                Toast.makeText(context, "Highest rated selected.", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.w(LOG_TAG, "Menu selection is not handled. ItemId: " + itemId);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
