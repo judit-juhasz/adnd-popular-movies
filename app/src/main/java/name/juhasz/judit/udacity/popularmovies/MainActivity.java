e43fa3fad624c5f236498d47a166efddd10b4ac7
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-10-04 00:51:46 +0200

    Prepare the adapter to make in updateable

--- app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/popularmovies/MainActivity.java
@@ -25,7 +25,18 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         mMoviesRecycleView = (RecyclerView) findViewById(R.id.rv_movies);
 
-        final MovieAdapter adapter = new MovieAdapter(this);
+        Movie[] dummyData = {
+                new Movie("Rogue One", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SY1000_SX675_AL_.jpg"),
+                new Movie("The Dark Knight", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
+                new Movie("Pulp Fiction", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SY1000_CR0,0,673,1000_AL_.jpg"),
+                new Movie("Passangers", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4MjU3MDIzOF5BMl5BanBnXkFtZTgwMjM2MzY2MDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
+                new Movie("Forrest Gump", "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg"),
+                new Movie("Moana", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
+                new Movie("Fight Club", "https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY999_CR0,0,704,999_AL_.jpg"),
+                new Movie("Frozen", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg")
+        };
+
+        final MovieAdapter adapter = new MovieAdapter(dummyData);
         mMoviesRecycleView.setAdapter(adapter);
 
         final int spanCount = 2;
--- app/src/main/java/name/juhasz/judit/udacity/popularmovies/MovieAdapter.java
+++ app/src/main/java/name/juhasz/judit/udacity/popularmovies/MovieAdapter.java
@@ -2,6 +2,7 @@ package name.juhasz.judit.udacity.popularmovies;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,24 +17,22 @@ import com.squareup.picasso.Picasso;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
-    private static final Movie[] sDummyData = {
-            new Movie("Rogue One", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SY1000_SX675_AL_.jpg"),
-            new Movie("The Dark Knight", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
-            new Movie("Pulp Fiction", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SY1000_CR0,0,673,1000_AL_.jpg"),
-            new Movie("Passangers", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4MjU3MDIzOF5BMl5BanBnXkFtZTgwMjM2MzY2MDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
-            new Movie("Forrest Gump", "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg"),
-            new Movie("Moana", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
-            new Movie("Fight Club", "https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY999_CR0,0,704,999_AL_.jpg"),
-            new Movie("Frozen", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg")
-    };
+    private final static String LOG_TAG = MovieAdapter.class.getSimpleName();
 
-    private MovieOnClickListener mListener;
+    private Movie[] mMovies;
+
+    public MovieAdapter(Movie[] movies) {
+        mMovies = movies;
+    }
+
+    private MovieAdapter.MovieOnClickListener mListener;
 
     public interface MovieOnClickListener {
         void onItemClick(Movie movie);
     }
 
-    public MovieAdapter(MovieOnClickListener listener) {
+    public MovieAdapter(MovieAdapter.MovieOnClickListener listener) {
+        mMovies = null;
         mListener = listener;
     }
 
@@ -54,40 +53,49 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
-        holder.bind(position);
+        final Movie movie = mMovies[position];
+        holder.bind(movie);
     }
 
     @Override
     public int getItemCount() {
-        return sDummyData.length;
+        if (mMovies == null) {
+            return 0;
+        } else {
+            return mMovies.length;
+        }
     }
 
     public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         private final Context mContext;
 
-        public ImageView moviePoster;
+        private ImageView mMoviePoster;
 
         public MovieViewHolder(Context context, View itemView) {
             super(itemView);
 
             mContext = context;
 
-            moviePoster = (ImageView) itemView.findViewById(R.id.tv_movie_poster);
-            moviePoster.setOnClickListener(this);
+            mMoviePoster = (ImageView) itemView.findViewById(R.id.tv_movie_poster);
+            mMoviePoster.setOnClickListener(this);
         }
 
-        void bind(int position) {
-            final Movie movie = sDummyData[position];
+        void bind(Movie movie) {
             final String posterPath = movie.getPosterPath();
-            Picasso.with(mContext).load(posterPath).into(moviePoster);
+            Picasso.with(mContext).load(posterPath).into(mMoviePoster);
         }
 
         @Override
         public void onClick(View view) {
-            int adapterPosition = getAdapterPosition();
-            Movie movie = sDummyData[adapterPosition];
-            mListener.onItemClick(movie);
+            if (null != mMovies) {
+                int adapterPosition = getAdapterPosition();
+                Movie movie = mMovies[adapterPosition];
+                mListener.onItemClick(movie);
+            } else {
+                Log.wtf(LOG_TAG, "OnClick handler call with empty movie list.");
+            }
+
         }
     }
 }
